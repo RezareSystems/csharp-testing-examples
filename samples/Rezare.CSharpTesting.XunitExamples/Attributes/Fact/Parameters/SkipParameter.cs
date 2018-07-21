b68977fb53c70c59e4048ee361c0e51281c8af72
Robert Larkins <ayb4btu@gmail.com>
2018-07-21 23:38:56 +1200

    Add Xunit Fact Skip parameter examples

---
+++ samples/Rezare.CSharpTesting.XunitExamples/Attributes/Fact/Parameters/SkipParameter.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Rezare.CSharpTesting.XunitExamples.Attributes.Fact.Parameters
+{
+    public class SkipParameter
+    {
+        [Fact(Skip = "Skip passing test")]
+        public void SkipFactTest_SkipPassingTestWithReason_TestSkippedWithReasonInOutput()
+        {
+            Assert.True(true);
+        }
+
+        [Fact(Skip = "Skip failing test")]
+        public void SkipFactTest_SkipFailingTestWithReason_TestSkippedWithReasonInOutput()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(Skip = "")]
+        public void SkipFactTest_SkipPassingTestWithEmptyReason_TestRunsAndPasses()
+        {
+            Assert.True(true);
+        }
+
+        [Fact(Skip = "")]
+        public void SkipFactTest_SkipFailingTestWithEmptyReason_TestRunsAndFails()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(Skip = "Empty test")]
+        public void SkipFactTest_SkipEmptyTest_TestIsSkipped()
+        {
+        }
+    }
+}
