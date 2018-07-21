4f760d1d2a7a14ecc47dd81418461c9245e57d24
Robert Larkins <ayb4btu@gmail.com>
2018-07-21 23:38:14 +1200

    Add Xunit Fact DisplayName parameter example

---
+++ samples/Rezare.CSharpTesting.XunitExamples/Attributes/Fact/Parameters/DisplayNameParameter.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Rezare.CSharpTesting.XunitExamples.Attributes.Fact.Parameters
+{
+    /// <summary>
+    /// The DisplayName for a test will only appear in the Visual Studio Test Explorer,
+    /// not in the ReSharper Unit Test Sessions window.
+    /// </summary>
+    public class DisplayNameParameter
+    {
+        [Fact(DisplayName = "Display new name for Fact")]
+        public void TestDisplayName_ChangeDisplayName_TestDisplaysNewName()
+        {
+            Assert.True(true);
+        }
+    }
+}
