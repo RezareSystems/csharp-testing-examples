65ba03d19461a754a9a33a83134d8b61237747cf
Robert Larkins <robert.larkins@rezare.co.nz>
2018-09-07 18:06:28 +1200

    Add Be extension examples with because message

--- samples/Rezare.CSharpTesting.FluentAssertionsExamples/Should/BeExtension.cs
+++ samples/Rezare.CSharpTesting.FluentAssertionsExamples/Should/BeExtension.cs
@@ -8,28 +8,39 @@ namespace Rezare.CSharpTesting.FluentAssertionsExamples.Should
         [Theory]
         [InlineData("test", "test")]
         [InlineData("trailing whitespace   ", "trailing whitespace   ")]
-        public void BeExtensionExample_StringType_AssertStringsAreExactlyTheSame(
+        public void Be_StringCheck_ExactlyTheSame(
             string result,
             string expected)
         {
             result.Should().Be(expected);
         }
 
+        [Trait("Demonstration Tests", "Purposely Failing")]
         [Fact]
-        public void BeExtensionExample_StringTypeWithBecause_AssertStringsAreExactlyTheSame()
+        public void Be_StringCheckWithBecause_ExactlyTheSame_FailPurposely()
         {
-            var becauseMessage = "the strings should be exactly the same";
+            var becauseMessage = "numbers go before letters";
 
-            "test123".Should().Be("test123", becauseMessage);
+            "test123".Should().Be("123test", becauseMessage);
         }
 
         [Trait("Demonstration Tests", "Purposely Failing")]
         [Fact]
-        public void BeExtensionExample_StringTypeWithBecause_AssertStringsAreExactlyTheSame_FailPurposely()
+        public void Be_StringCheckWithBecauseAndParams_ExactlyTheSame_FailPurposely()
         {
-            var becauseMessage = "the strings should be exactly the same";
+            var becauseMessage = "{0} should be {1} before {2}";
+            var becauseParams = new object[] { typeof(string), "numbers", "letter" };
 
-            "test123".Should().Be("123test", becauseMessage);
+            "123test".Should().Be("123test", becauseMessage, becauseParams);
+        }
+
+        [Trait("Demonstration Tests", "Purposely Failing")]
+        [Fact]
+        public void Be_StringCheckWithBecauseAndParamsArray_ExactlyTheSame_FailPurposely()
+        {
+            var becauseMessage = "{0} should be {1} before {2}";
+
+            "test123".Should().Be("123test", becauseMessage, typeof(string), "numbers", "letter");
         }
     }
 }
