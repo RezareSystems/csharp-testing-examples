f5f016478b93d0d07700caf419e6953a8e729e77
Robert Larkins <robert.larkins@rezare.co.nz>
2018-09-07 17:25:17 +1200

    Add example of FluentAssertions Be extension

---
+++ samples/Rezare.CSharpTesting.FluentAssertionsExamples/Should/BeExtension.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Rezare.CSharpTesting.FluentAssertionsExamples.Should
+{
+    public class BeExtension
+    {
+        [Theory]
+        [InlineData("test", "test")]
+        [InlineData("trailing whitespace   ", "trailing whitespace   ")]
+        public void BeExtensionExample_StringType_AssertStringsAreExactlyTheSame(
+            string result,
+            string expected)
+        {
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void BeExtensionExample_StringTypeWithBecause_AssertStringsAreExactlyTheSame()
+        {
+            var becauseMessage = "the strings should be exactly the same";
+
+            "test123".Should().Be("test123", becauseMessage);
+        }
+
+        [Trait("Demonstration Tests", "Purposely Failing")]
+        [Fact]
+        public void BeExtensionExample_StringTypeWithBecause_AssertStringsAreExactlyTheSame_FailPurposely()
+        {
+            var becauseMessage = "the strings should be exactly the same";
+
+            "test123".Should().Be("123test", becauseMessage);
+        }
+    }
+}
