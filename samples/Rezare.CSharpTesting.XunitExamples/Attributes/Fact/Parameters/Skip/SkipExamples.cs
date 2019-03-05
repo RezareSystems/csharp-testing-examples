5cd9da5d29e13c2cb36a1aafe03b1b45a6bb86ae
Robert Larkins <ayb4btu@gmail.com>
2019-03-05 21:02:51 +1300

    Refactor SkipExamples
    The following has been done:
    
     - Add xml docs to each method
    
     - Change each method to an expression body where suitable
    
     - Rename each method to be clearer
    
     - Add Skip = null example

--- samples/Rezare.CSharpTesting.XunitExamples/Attributes/Fact/Parameters/Skip/SkipExamples.cs
+++ samples/Rezare.CSharpTesting.XunitExamples/Attributes/Fact/Parameters/Skip/SkipExamples.cs
@@ -1,36 +1,60 @@
 ﻿using Xunit;
 
-namespace Rezare.CSharpTesting.XunitExamples.Attributes.Fact.Parameters
+namespace Rezare.CSharpTesting.XunitExamples.Attributes.Fact.Parameters.Skip
 {
+    /// <summary>
+    /// Examples of how to use the Skip attribute.
+    /// </summary>
     public class SkipExamples
     {
+        /// <summary>
+        /// Skips a passing test with the provided reason being displayed in output.
+        /// </summary>
         [Fact(Skip = "Skip passing test")]
-        public void SkipPassingTestWithReason_TestSkippedWithReasonInOutput()
-        {
-            Assert.True(true);
-        }
+        public void SkipPassingTest_WithReason_TestSkippedWithReasonInOutput() => Assert.True(true);
 
+        /// <summary>
+        /// Skips a failing test with the provided reason being displayed in output.
+        /// </summary>
         [Fact(Skip = "Skip failing test")]
-        public void SkipFailingTestWithReason_TestSkippedWithReasonInOutput()
-        {
-            Assert.True(false);
-        }
+        public void SkipFailingTest_WithReason_TestSkippedWithReasonInOutput() => Assert.True(false);
 
+        /// <summary>
+        /// Runs a passing test as it has an empty Skip reason.
+        /// </summary>
+        /// <remarks>
+        /// The Xunit TestRunner uses <c>string.IsNullOrEmpty(SkipReason)</c> to determine if a test should be run.
+        /// Meaning that tests with an empty string for the Skip Reason are not skipped.
+        /// </remarks>
         [Fact(Skip = "")]
-        public void SkipPassingTestWithEmptyReason_TestRunsAndPasses()
-        {
-            Assert.True(true);
-        }
+        public void SkipPassingTest_EmptyReason_TestRunsAndPasses() => Assert.True(true);
 
+        /// <summary>
+        /// Runs a failing test as it has an empty Skip reason.
+        /// </summary>
+        /// <remarks>
+        /// The Xunit TestRunner uses <c>string.IsNullOrEmpty(SkipReason)</c> to determine if a test should be run.
+        /// Meaning that tests with an empty string for the Skip Reason are not skipped.
+        /// </remarks>
         [Fact(Skip = "")]
-        public void SkipFailingTestWithEmptyReason_TestRunsAndFails()
-        {
-            Assert.True(false);
-        }
+        public void SkipFailingTest_EmptyReason_TestRunsAndFails() => Assert.True(false);
 
+        /// <summary>
+        /// Test with Skip set to null.
+        /// </summary>
+        /// <remarks>
+        /// Null is the default value for Skip, so Skip is ignored.
+        /// </remarks>
+        [Fact(Skip = null)]
+        public void SkipPassingTest_SkipIsNull_TestRunsAndPasses() => Assert.True(true);
+
+        /// <summary>
+        /// Skips an empty test with a reason.
+        /// </summary>
         [Fact(Skip = "Empty test")]
-        public void SkipEmptyTest_TestIsSkipped()
+        public void SkipEmptyTest_WithReason_TestIsSkipped()
         {
+            // Demonstrate how an empty skipped test operates.
         }
     }
 }
